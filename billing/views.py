6aa7e7783676625a593ba785eae1a566d8bf8439
Jason Myers <jason@jasonamyers.com>
2013-10-19 13:38:40 -0500

    Plans view
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

--- billing/views.py
+++ billing/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from vatio.decorators import render_to
+from .models import Plan
+
+
+@render_to('vatio/index.html')
+def plans_selection(request):
+    plans = Plan.objects.all()
+    return {
+        'plans': plans
+    }
