bdbf351cbc5001f04581a841389e021061293a29
Jason Myers <jason@jasonamyers.com>
2013-10-19 01:22:45 -0500

    Typos eneeds sleepsy
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 from accounts.models import Organization, Department, Profile
 
-admin.site.register(Oraganization, Department, Profile)
+admin.site.register(Organization, Department, Profile)
