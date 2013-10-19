584c53216c866eb3375de15001f3d7eccd7a38be
Jason Myers <jason@jasonamyers.com>
2013-10-19 02:50:43 -0500

    adding index url
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

--- vatio/urls.py
+++ vatio/urls.py
@@ -4,14 +4,12 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'vatio.views.home', name='home'),
+     url(r'^$', 'vatio.views.index', name='index'),
     # url(r'^vatio/', include('vatio.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
