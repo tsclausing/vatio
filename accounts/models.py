c5da8d85b4cb8b5ea1608e5b98a7ff2ea07ddad8
Jason Myers <jason@jasonamyers.com>
2013-10-19 01:06:50 -0500

    Added Profile Department Org
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 
-# Create your models here.
+import simple_audit
+
+
+class Organization(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, unique=True)
+    phone = models.CharField(max_length=15, blank=True)
+    org = models.OneToOneField(Organization, null=True)
+    dept = models.ManyToManyField(Organization, null=True)
+    is_org_admin = models.BooleanField(default=False)
+    timezone = models.CharField(max_length=255, blank=True)
+
+
+def create_user_profile(sender, instance, created, **kwargs):
+    """ Don't create when testing or using loaddata.
+    See http://stackoverflow.com/questions/3499791/how-do-i-prevent-fixtures-from-conflicting-with-django-post-save-signal-code """
+    if created and not kwargs.get('raw', False):
+        Profile.objects.create(user=instance)
+
+post_save.connect(create_user_profile, sender=User, dispatch_uid='0a0bd29e01d9418fb9e20635a7761800')
+
+simple_audit.register(Organization, Department, Profile)
