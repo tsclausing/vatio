bb412612a6c5e0588c9879ecf7aa4417454354f4
Patrick Cason <me@patrickcason.com>
2013-10-19 13:49:45 -0500

    Pushing CSS branch, foundation_tags broken

---
+++ config.rb
@@ -0,0 +1,25 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "vatio/static/css"
+sass_dir = "vatio/static/scss"
+images_dir = "vatio/static/images"
+javascripts_dir = "vatio/static/js"
+fonts_dir = "vatio/static/fonts"
+
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass gunslinger/static/gunslinger/scss scss && rm -rf sass && mv scss sass
--- vatio/views.py
+++ vatio/views.py
@@ -1,6 +1,6 @@
 from vatio.decorators import render_to
 
 
-@render_to('vatio/index.html')
+@render_to('vatio/home.html')
 def index(request):
     return {}
