a770f309895eca714b951c13f7771f97ef215b5f
Jason Myers <jason@jasonamyers.com>
2013-10-19 00:02:44 -0500

    Media URLs
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

--- vatio/urls.py
+++ vatio/urls.py
@@ -13,7 +13,9 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.MEDIA_ROOT,
+    }),
     # Uncomment the next line to enable the admin:
      url(r'^admin/', include(admin.site.urls)),
      urlpatterns += staticfiles_urlpatterns()
