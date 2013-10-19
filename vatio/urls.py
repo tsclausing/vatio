2bb4d54f953b40a1d6cc700683d623b0ca47cd57
Jason Myers <jason@jasonamyers.com>
2013-10-19 00:13:17 -0500

    Importing settings
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

--- vatio/urls.py
+++ vatio/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
+from django.conf import settings
+
+
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
