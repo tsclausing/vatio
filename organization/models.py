cc15e62819b2ba14d3e17d003594e4e011b082ca
Jason Myers <jason@jasonamyers.com>
2013-10-20 12:37:48 -0500

    cookies
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

--- organization/models.py
+++ organization/models.py
@@ -6,12 +6,18 @@ from django.db import models
 class Organization(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
+
 
 class Department(models.Model):
     name = models.CharField(max_length=255)
-    org = models.OneToOneField(Organization, blank=False, null=True)
+    org = models.ManyToManyField(Organization, blank=False, null=True)
     dept = models.ForeignKey('Department', null=True, blank=True)
 
 
+    def __str__(self):
+        return ' - '.join([self.name, self.org, self.dept])
 
 #simple_audit.register(Organization, Department)
