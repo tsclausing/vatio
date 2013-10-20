3755ba32a11dfeaee7591d17408a86c41e8f3709
tsclausing <tsclausing@gmail.com>
2013-10-20 11:32:04 -0500

    Bring Billing API in line with the others.

--- billing/urls.py
+++ billing/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns(
     '',
-    url(r'^plans/all/$', 'billing.views.plans', name='plans'),
-    url(r'^plans/(\d+)/$', 'billing.views.plans', name='plans'),
+    url(r'^all/json/$', 'billing.views.plans_json', name='plans_json'),
+    url(r'^(\d+)/json/$', 'billing.views.plans_json', name='plans_json'),
+    url(r'^all/$', 'billing.views.plans_view', name='plans_view'),
+    url(r'^(\d+)/$', 'billing.views.plans_view', name='plans_view'),
 )
--- billing/views.py
+++ billing/views.py
@@ -1,21 +1,25 @@
-from vatio.decorators import render_json
+from vatio.decorators import render_json, render_to
 from .models import Plan
 
 
+def build_plan(pk):
+    plans = Plan.objects.filter(pk=pk) if pk else Plan.objects.all()
+    return {
+        'plans': [{
+            'id': x.id,
+            'max_users': x.max_users,
+            'name': x.name,
+            'teams': x.teams,
+            'user_cost': float(x.user_cost),
+            'fixed_cost': float(x.fixed_cost),
+        } for x in plans]
+    }
+
+
 @render_json()
-def plans(request, id=None):
-    if request.method == 'GET':
-        if id:
-            plans = Plan.objects.filter(pk=id)
-        else:
-            plans = Plan.objects.all()
-        return {
-            'plans': [{
-                'id': x.id,
-                'max_users': x.max_users,
-                'name': x.name,
-                'teams': x.teams,
-                'user_cost': float(x.user_cost),
-                'fixed_cost': float(x.fixed_cost),
-            } for x in plans]
-        }
+def plans_json(request, id=None):
+    return build_plan(id)
+
+@render_to('')
+def plans_view(request, id=None):
+    return build_plan(id)
